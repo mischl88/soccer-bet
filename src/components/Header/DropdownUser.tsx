b29a3a4e8aad96ffae83898096fe8766774591ac
mikee <>
2024-05-29 00:38:25 +0200

    fix

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -57,7 +57,7 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-bodydark1">
-            {user?.username}
+            {/*{user?.username}*/}
           </span>
         </span>
 
