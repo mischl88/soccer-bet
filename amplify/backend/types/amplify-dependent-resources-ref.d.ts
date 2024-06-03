69cc94541e257af0d485a7356498513c3fe35f4f
mikee <>
2024-06-03 10:54:13 +0200

    new amplify config

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,20 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "soccerappauth": {
+    "soccerbetb6135db2": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "soccerappauthPostConfirmation": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
     }
   }
 }
